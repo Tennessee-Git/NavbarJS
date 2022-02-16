1bb9e2e12f5c0db02b99ad0e14982708ec7097c7
Paweł Awramiuk <awramiukofficial@gmail.com>
2022-02-16 09:10:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', () => {
+    // if(links.classList.contains('show-links')) {
+    //     links.classList.remove('show-links');
+    // }
+    // else {
+    //     links.classList.add('show-links');
+    // }
+    // Monke metoda ^
+    links.classList.toggle('show-links');
+})
